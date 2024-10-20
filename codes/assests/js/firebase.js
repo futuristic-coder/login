21d5c26318497e35067b0c0e54394a2bb2e5f643
sahil25123 <sahilgupta25123@gmail.com>
2024-10-20 21:27:08 +0530

    lastest changes

--- codes/assests/js/firebase.js
+++ codes/assests/js/firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBFRyu8a0OPfHHQpx7eIpe038xYfgk3bU4",
-    authDomain: "login-49bea.firebaseapp.com",
-    projectId: "login-49bea",
-    storageBucket: "login-49bea.appspot.com",
-    messagingSenderId: "983307995336",
-    appId: "1:983307995336:web:bd4e952543494bb4087dfd",
-    measurementId: "G-Q1TTV28Y9M"
+  apiKey: "AIzaSyBFRyu8a0OPfHHQpx7eIpe038xYfgk3bU4",
+  authDomain: "login-49bea.firebaseapp.com",
+  projectId: "login-49bea",
+  storageBucket: "login-49bea.appspot.com",
+  messagingSenderId: "983307995336",
+  appId: "1:983307995336:web:bd4e952543494bb4087dfd",
+  measurementId: "G-Q1TTV28Y9M"
+    
 };
 
 // Initialize Firebase
